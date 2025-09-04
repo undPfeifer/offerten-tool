d4ce81eb1be4db424a7dbaad0cbc2d92f503b162
undPfeifer <dimitri.pfeifer@gmx.net>
2025-09-04 11:36:16 +0200

    pre appify

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,12 +25,23 @@
       class="column--element"
       v-for="(element, elIndex) in column.elements"
       :key="elIndex"
-      @click="toggleElement(column, element)"
       :class="{ disabled: !element.enabled }"
     >
-      <p class="element--name">{{ element.name }}</p>
+      <p class="element--name"
+      @click="toggleElement(column, element)"
+      :class="{
+        'low-hours': element.hours <= 2,
+        'medium-hours': element.hours > 2 && element.hours <= 5,
+        'high-hours': element.hours > 5
+      }"
+
+      >{{ element.name }}</p>
       <div class="element--hours">
-        <p>{{ getEffectiveHours(element) }}h</p>
+        <p
+        @click="addHours(element, $event)"
+
+
+        >{{ getEffectiveHours(element) }}h</p>
       </div>
     </div>
 
@@ -39,7 +50,7 @@
       style="width: calc(100% - 34px); display: flex; flex-direction: column; align-items: center; gap: 10px;"
     >
       <svg
-        style="width: 30px;"
+        style="width: 20px;"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 32 32"
       >
@@ -67,7 +78,10 @@
     >multi-site</button>
 
 
+</div>
+<div class="preis--wrapper">
         <section class="preis--container">
+
             <div>
                 <p>Stundenaufwand</p>
                 <h1>{{ totalHours }}h</h1>
@@ -87,8 +101,8 @@
                 v-else
                 >{{ Math.floor((columnSum * 0.85)/10) *10}}.-</h1>
             </div>
+            
         </section>
-        <p class="annotations">*Weil grössere Aufträge viel Zeit in Administration und Akquise sparen, können sie kostengünstiger angeboten werden.</p>
     </div>
 
 
@@ -98,11 +112,33 @@
  </section>
 </section>
 
-<p class="foothair"> 03.09.25 <span>&pfeifer </span> </p>
+<p class="annotations">*Weil grössere Aufträge viel Zeit in Administration und Akquise sparen, können sie kostengünstiger angeboten werden.</p>
+
+
+<p class="foothair"> {{today}} <span>&pfeifer </span> </p>
 
 </template>
 
 <script setup>
+
+function addHours (element , event) {
+    if (event.shiftKey) {
+
+        element.hours-=1;
+        if (element.hours<0) {
+            element.hours=0;
+        }
+    }
+
+    else {
+        element.hours+=1;
+
+    }
+}
+
+
+
+
 const buttonState = ref('multi') // or ref(true/false)
 
 // Fix toggle function
@@ -145,8 +181,8 @@ const columns = ref([
         name: "Research / Marketing",
         elements: [
             { name: "Wer sind wir?", hours: 1, enabled: true },
-            { name: "Wie sind wir?", hours: 0.5, enabled: true },
-            { name: "Für wen sind wir?", hours: 0.5, enabled: true },
+            { name: "Wie sind wir?", hours: 1, enabled: true },
+            { name: "Für wen sind wir?", hours: 1, enabled: true },
             { name: "Zielgruppe definieren", hours: 1, enabled: true },
             { name: "Konkurrenzanalyse", hours: 2, enabled: true },
         ],
@@ -159,37 +195,44 @@ const columns = ref([
             { name: "Logo", hours: 6, enabled: true },
             { name: "Gestaltungskonzept Layout", hours: 3, enabled: true },
             { name: "Brand Guidelines", hours: 3, enabled: true },
-            { name: "Gestaltungs-Elemente (Icons etc)", hours: 2, enabled: true },
+            { name: "Icons", hours: 2, enabled: true },
             { name: "Bildwelt definieren", hours: 3, enabled: true },
         ],
     },
     {
         name: "Anwendungen",
         elements: [
-            { name: "Website Struktur & Gestaltung", hours: 4, enabled: true },
-            { name: "Website umsetzen", hours: 6, enabled: true },
-            { name: "Website Unterseiten umsetzen", hours: 5, enabled: true },
+            { name: "Web Struktur & Gestaltung", hours: 6, enabled: true },
+            { name: "Website umsetzen", hours: 8, enabled: true },
+            { name: "Web Unterseiten", hours: 2, enabled: true },
             { name: "Website Testen und optimieren", hours: 3, enabled: true },
-            { name: "Portraits fotografieren", hours: 2, enabled: true },
+            { name: "Portraits fotografieren", hours: 1, enabled: true },
             { name: "Texte schreiben", hours: 3, enabled: true },
-            { name: "Bildwelt fotografieren & 3d Render", hours: 5, enabled: true },
+            { name: "Bildwelt & 3d Render", hours: 5, enabled: true },
         ],
     },
     {
         name: "Vorlagen*",
         elements: [
-            { name: "Offerten", hours: 2, enabled: true },
-            { name: "Datenblätter", hours: 2, enabled: true },
-            { name: "Visitenkarten, Sticker usw", hours: 2, enabled: true },
+            { name: "Offerten", hours: 1, enabled: true },
+            { name: "Datenblätter", hours: 1, enabled: true },
+            { name: "Visitenkarten, Sticker usw", hours: 1, enabled: true },
             { name: "Rechnungen", hours: 1, enabled: true },
-            { name: "Fotostudio einrichten", hours: 3, enabled: true },
-            { name: "Bildbearbeitung Vorlagen", hours: 3, enabled: true },
+            { name: "Fotostudio einrichten", hours: 2, enabled: true },
+            { name: "Bildbearbeitung Vorlagen", hours: 1, enabled: true },
         ],
     },
 ]);
 
 const seitenzahl = ref(1);
-seitenzahl.value= 1.5;
+seitenzahl.value= 2;
+
+
+
+
+//data
+
+let today = new Date().toLocaleDateString()
     
 </script>
 
@@ -200,257 +243,273 @@ seitenzahl.value= 1.5;
     opacity: 0.5;
     transform: scaleX(0.93);
     background-color: azure;
+    height: auto;
 
 }
-
 
 /* #endregion */
 
 /* #region BASESTYLES */
 
+html {
+    font-size: 13px;
+}
+
+* {
+    user-select: none;
 
 
-    * {
-        font-family: Arial, Helvetica, sans-serif;
-        margin: 0px;
-        padding: 0px;
-    }
 
-    body {
-        padding: 10px 40px 20px 40px;
-    }
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0rem;
+    padding: 0rem;
+}
 
+body {
+    padding: 0.625rem 2.5rem 20rem 2.5rem;
+}
 
-    button {
-        border: none;
-        margin: 0;
-        padding: 0;
-        text-align: inherit;
-        font: inherit;
-        border-radius: 0;
+button {
+    border: none;
+    margin: 0rem;
+    padding: 0rem;
+    text-align: inherit;
+    font: inherit;
+    border-radius: 0rem;
 
-        cursor: pointer;
+    cursor: pointer;
 
-        background-color: black;
-        color: white;
+    background-color: black;
+    color: white;
 
-        font-size: 14px;
-        padding: 6px;
-        border: 1px solid white;
-    }
+    font-size: 0.875rem;
+    padding: 0.375rem;
+    border: 0.0625rem solid white;
+}
 
-    button:hover {
-        opacity: .8;
-    }
+button:hover {
+    opacity: 0.8;
+}
 
-    button.inactive {
-        opacity: 0.3;
-    }
+button.inactive {
+    opacity: 0.3;
+}
 
-    .annotations {
-        font-size: 11px;
-        opacity: 0.5;
-        position: fixed;
-        bottom: 10px;
-    }
+.annotations {
+    font-size: 0.6875rem;
+    opacity: 0.5;
+    position: fixed;
+    bottom: 0.625rem;
+}
 
-    .body--container{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    /* #endregion */
+.body--container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
-
+/* #endregion */
 
 /* #region typography */
 
-    h1 {
-        font-size: 44px;
-        font-weight: 900;
-        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        letter-spacing: -1px;
-    }
-
-    input {
-        all: unset;
-        font-size: 44px;
-        font-weight: 900;
-        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        letter-spacing: -1px;
-        margin: 0px;
-        padding: 0px 10px 0px 10px; 
-        height: fit-content;
-        caret-color: green;
-        color: #4CAF50;
-
-  }
-
-  input:focus {
-    color: black;
-    animation: blinkBorder 1s infinite;
-
-  }
-  @keyframes blinkBorder {
-  0%, 100% { color: #4CAF50; }
-  50% { color: #68ce6b; }
+h1 {
+    font-size: 2.75rem;
+    font-weight: 900;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    letter-spacing: -0.0625rem;
 }
 
-    .eyebrow {
-        font-size: 14px;
-        margin-bottom: 10px;
-    }
+input {
+    all: unset;
+    font-size: 2.75rem;
+    font-weight: 900;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    letter-spacing: -0.0625rem;
+    margin: 0rem;
+    padding: 0rem 0.625rem;
+    height: fit-content;
+    caret-color: green;
+    color: #4CAF50;
+}
 
-    .eyebrow span {
-        font-weight: bold;
-        
-    }
+input:focus {
+    color: black;
+    animation: blinkBorder 1s infinite;
+}
 
-    .foothair {
-        font-size: 14px;
-        position: absolute;
-        bottom: 10px;
-        right: 30px;
-    }
+@keyframes blinkBorder {
+    0%, 100% { color: #4CAF50; }
+    50% { color: #68ce6b; }
+}
 
-    .foothair span {
-        font-weight: bold;
-        font-family: 'Times New Roman', Times, serif;
-        font-style: italic;
-        
-    }
+.eyebrow {
+    font-size: 0.875rem;
+    margin-bottom: 0.625rem;
+}
+
+.eyebrow span {
+    font-weight: bold;
+}
+
+.foothair {
+    font-size: 0.875rem;
+    position: absolute;
+    bottom: 0.625rem;
+    right: 1.875rem;
+}
+
+.foothair span {
+    font-weight: bold;
+    font-family: 'Times New Roman', Times, serif;
+    font-style: italic;
+}
+
 
 
 /* #endregion */
 
-
 /* #region CUSTOM ELEMENTS */
 
-    .main--grid{
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        gap: 16px;
-        max-width: 940px;
-    }
+.main--grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 1rem;
+    max-width: 58rem;
+}
 
-    .column--header{
-        font-size: 16px;
-        border-bottom: 1px solid black;
-        padding: 4px;
-        margin-bottom: 16px;
-        
-    }
+.column--header {
+    font-size: 1rem;
+    border-bottom: 0.0625rem solid black;
+    padding: 0.25rem;
+    margin-bottom: 0.8rem;
+}
 
-    
-    .column--element{
-        display: flex;
-        width: 100%;
-        align-items: center;
-        cursor: pointer;
+.column--element {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    cursor: pointer;
+}
 
-    }
+.column--element:hover .element--name {
+    border-radius: 0.625rem;
+    transform: scale(0.95);
+    background-image: linear-gradient(rgba(0, 186, 65, 0.154), rgba(0, 138, 21, 0));
+}
 
-    .column--element:hover .element--name {
-        border-radius: 10px;
-        transform: scale(0.95);
-        background-image: linear-gradient(rgba(0, 186, 65, 0.154), rgba(0, 138, 21, 0));
-        ;
-    }
+.element--name {
+    font-size: 0.875rem;
+    padding: 1.5rem 1rem;
+    text-align: center;
+    background-color: rgb(177, 229, 167);
+    width: 100%;
+    border-bottom: 0.0625rem solid black;
+    transition: 0.1s linear transform;
+    transition: 0.3s linear border-radius;
+}
 
-    .element--name{
-        font-size: 14px;
-        padding: 24px 16px;
-        text-align: center;
-        background-color: rgb(177, 229, 167);
-        width: 100%;
-        border-bottom: 1px solid black;
-        transition: .1s linear transform;
-        transition: .3s linear border-radius;
+.element--hours {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
+    color: white;
+    font-size: 0.875rem;
 
-    }
+    background-color: black;
+    width: 2.125rem;
+    height: 2.125rem;
+    border-radius: 9.375rem;
+    line-height: 0rem;
 
-    .element--hours {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    aspect-ratio: 1/1;
+}
 
-        color: white;
-        font-size: 14px;
+.element--hours p {
+    margin: 0rem;
+    padding: 0rem;
+}
 
-        background-color: black;
-        width: 34px;
-        height: 34px;
-        border-radius: 150px;
-        line-height: 0px;
+.element--hours:hover{
+    transform: scale(1.1);
+}
 
-        aspect-ratio: 1/1;
-        
+.columnSum {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
+    color: white;
+    font-size: 1rem;
+    font-weight: 600;
 
-    }
+    background-color: black;
+    width: 2.75rem;
+    height: 2.75rem;
+    border-radius: 9.375rem;
+    line-height: 0rem;
 
-    .element--hours p {
-        margin: 0px;
-        padding: 0px;
-    }
+    aspect-ratio: 1/1;
+}
 
+.macro--controls {
+    padding: 0rem;
+    position: fixed;
+    bottom: 7.25rem;
+    left: 10px;
+    width: 50rem;
+}
 
-    .columnSum {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+.macro--controls .p {
+    font-size: 1.5rem;
+    font-weight: 800;
+}
 
-        color: white;
-        font-size: 16px;
-        font-weight: 600;
-
-
-        background-color: black;
-        width: 44px;
-        height: 44px;
-        border-radius: 150px;
-        line-height: 0px;
-
-        aspect-ratio: 1/1;
-    }
-    
-
-
-
-
-    .macro--controls{
-        padding: 0px 0px;
-        position: fixed;
-        bottom: 36px;
-        width: 800px;
-    }
-
-    .macro--controls .p{
-        font-size: 24px;
-        font-weight: 800;
-    }
-
-    /* #endregion */
-
+/* #endregion */
 
 /* #region PREIS SECTION */
 .preis--container {
-        margin-top: 20px;
-
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-
-    }
-
-    .preis--container p {
-        font-weight: 600;
-        font-size: 14px;
-        border-bottom: 1px solid black;
-        padding: 0px 0px 3px 0px
-    }
-
-    /* #endregion */
+    margin-top: 1.25rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
 
 
+
+    max-width: 600px;
+
+
+}
+
+.preis--wrapper{
+    background-color: rgba(255, 255, 255, 0);
+    backdrop-filter: blur(50px);
+    position: fixed;
+    padding-bottom: 20px;
+    bottom: 0px;
+    width: 100%;
+
+}
+
+.preis--container p {
+    font-weight: 600;
+    font-size: 0.875rem;
+    border-bottom: 0.0625rem solid black;
+    padding: 0rem 0rem 0.1875rem 0rem;
+}
+/* #endregion */
+
+.low-hours {
+    background-color: rgb(177, 229, 167);
+    padding: 0.8rem 1rem;
+}
+
+.medium-hours {
+    background-color: #88d48a;
+    padding: 1.4rem 1rem;
+}
+
+.high-hours {
+    background-color: #4eb551;
+    padding: 1.9rem 1rem;
+}
 </style>
